e380f39d77822baf2621dde1b3d38ee0cd3321ee
Danny Infurna <f.danny95@gmail.com>
2024-02-29 17:46:31 +0100

    refactor of contact component

--- src/app/features/contact/contact.component.ts
+++ src/app/features/contact/contact.component.ts
@@ -1,54 +1,48 @@
-import {CommonModule} from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent {
+
+  fb = inject(FormBuilder);
+  router = inject(Router);
 
   regExpName:RegExp = /^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$/;
   regExpTel:RegExp = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/;
   regExpEmail:RegExp = /^[\-\w\.]+@([\-\w]+\.)+[\-\w]{2,4}$/;
 
-  constructor(
-    private fb: FormBuilder, // form builder dei form reactive
-    private route:Router
-  ) { }
-  
-  contactForm!: FormGroup;
+  contactForm = this.fb.nonNullable.group({
+    contactNome: ['',[Validators.required, Validators.minLength(5), Validators.pattern(this.regExpName)]],
+    contactEmail: ['',[Validators.required, Validators.email, Validators.pattern(this.regExpEmail)]],
+    contactTel: ['',[Validators.required, Validators.pattern(this.regExpTel)]],
+    contactMsg: ['',[Validators.required, Validators.minLength(20), Validators.maxLength(150)]],
+    privacy: [false, Validators.required],
+  });
 
-  isErrorVisible:boolean = false; // gestione span error/success
-  myMessage:string = '';
-  typeMessage:boolean = false;
-  
-
-  ngOnInit(): void {    
-     // reactive form con validazione per il form di registrazione
-     this.contactForm = this.fb.group({
-      contactNome: new FormControl ('',[Validators.required, Validators.minLength(5), Validators.pattern(this.regExpName)]),
-      contactEmail: new FormControl ('',[Validators.required, Validators.email, Validators.pattern(this.regExpEmail)]),
-      contactTel: new FormControl ('',[Validators.required, Validators.pattern(this.regExpTel)]),
-      contactMsg: new FormControl('',[Validators.required, Validators.minLength(20), Validators.maxLength(150)]),
-      privacy: new FormControl (false, Validators.required),
-    });
-  }
+  isErrorVisible = signal(false); 
+  myMessage = signal('');
+  typeMessage = signal(false);
 
   contact(){
-    this.typeMessage = true;
-    this.isErrorVisible = true;
-    this.myMessage = 'Messaggio inviato con successo'
+    this.typeMessage.set(true);
+    this.isErrorVisible.set(true);
+    this.myMessage.set('Messaggio inviato con successo');
 
     setTimeout(() => {
-        this.isErrorVisible = false;
-        this.myMessage = '';
-        this.typeMessage = false;
-        this.route.navigate(['/home'])
+        this.isErrorVisible.set(false);
+        this.myMessage.set('');
+        this.typeMessage.set(false);
+
+        this.router.navigate(['/home'])
     }, 2000); 
   }
 
