533629d6d6452dbce5c1cf7e3e72930e6bb14e83
Danny Infurna <f.danny95@gmail.com>
2024-03-01 09:04:07 +0100

    feat: added the navigation to login if jwt expires

--- src/app/interceptor/auth-interceptor.interceptor.ts
+++ src/app/interceptor/auth-interceptor.interceptor.ts
@@ -1,7 +1,11 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { inject} from '@angular/core';
+import { Router} from '@angular/router';
+import { catchError, throwError } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  
+  const router = inject(Router);
+
   if(localStorage.getItem('token')){
     let newReq = req.clone({
         headers: req.headers
@@ -9,6 +13,13 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
         .set('Authorization', `Bearer ${localStorage.getItem('token')}`)
     })
 
-    return next(newReq)
+    return next(newReq).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          router.navigate(['/login']);
+        }
+        return throwError(()=> new Error(error.message));
+      })
+    );
   } else return next(req);
 };
