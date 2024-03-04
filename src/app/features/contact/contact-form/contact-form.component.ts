780b063ec02cf8930a14583c54d1ba7a8fad3d10
Danny Infurna <f.danny95@gmail.com>
2024-03-04 09:43:17 +0100

    feat: optimize contact component

---
+++ src/app/features/contact/contact-form/contact-form.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, Output, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import { REGEX_NAME, REGEX_EMAIL, REGEX_TEL } from '../../../costants';
+
+@Component({
+  selector: 'app-contact-form',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './contact-form.component.html',
+  styleUrl: './contact-form.component.scss'
+})
+export class ContactFormComponent {
+
+  @Output() sendEmail = new EventEmitter()
+  
+  fb = inject(FormBuilder);
+
+  contactForm = this.fb.nonNullable.group({
+    nome: ['', [Validators.required, Validators.minLength(5), Validators.pattern(REGEX_NAME)]],
+    email: ['', [Validators.required, Validators.email, Validators.pattern(REGEX_EMAIL)]],
+    tel: ['', [Validators.required, Validators.pattern(REGEX_TEL)]],
+    msg: ['', [Validators.required, Validators.minLength(20), Validators.maxLength(150)]],
+    privacy: [false, Validators.requiredTrue],
+  });
+}
--- src/app/features/contact/contact.component.ts
+++ src/app/features/contact/contact.component.ts
@@ -1,32 +1,53 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { AlertService } from '../../shared/components/alert.service';
-import {REGEX_EMAIL, REGEX_NAME, REGEX_TEL} from '../../costants';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CommonModule, ReactiveFormsModule],
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  imports: [CommonModule, ContactFormComponent],
+  template: `
+    <div id="contact">
+    <img src="./../../assets/footer.png" alt="logo-footer" />
+
+    <div class="chi-siamo">
+      <app-contact-form (sendEmail)="sendEmail()"></app-contact-form>
+    </div>
+  </div>
+  `,
+  styles: `
+    #contact{
+    display: flex;
+    width: 100%;
+    justify-content: center;
+
+      img{
+        margin: 20px 100px;
+        max-height: 500px;
+        align-self: center;
+      }
+    }
+
+    .chi-siamo{
+      max-width: 600px;
+      align-self: center;
+    }
+
+    @media screen and (max-width: 1100px) {
+        #contact{
+            flex-direction: column;
+        }
+    }
+  `
 })
 export class ContactComponent {
 
-  fb = inject(FormBuilder);
   router = inject(Router);
   alertService = inject(AlertService);
-
-  contactForm = this.fb.nonNullable.group({
-    nome: ['', [Validators.required, Validators.minLength(5), Validators.pattern(REGEX_NAME)]],
-    email: ['', [Validators.required, Validators.email, Validators.pattern(REGEX_EMAIL)]],
-    tel: ['', [Validators.required, Validators.pattern(REGEX_TEL)]],
-    msg: ['', [Validators.required, Validators.minLength(20), Validators.maxLength(150)]],
-    privacy: [false, Validators.requiredTrue],
-  });
 
   sendEmail(){
     this.alertService.showAlert('success', 'Messaggio inviato con successo!')
